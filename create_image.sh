3388e5535058fc602d4937a2f68eacbdfc463635
Thorben Wilde <thorben.wilde@gmx.de>
2021-10-25 18:38:17 +0200

    Improve create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
+cd `dirname "$0"`
+
+# enshure that relevant tools are executable
+chmod +x XR809SDK/tools/mkimage
+chmod +x XR809SDK/tools/phoenixMC
+
 cd build_output
 ../XR809SDK/tools/mkimage -O -c ../XR809SDK/project/image_cfg/xr809/image_sta_ap_xip.cfg -o xr_system.img
